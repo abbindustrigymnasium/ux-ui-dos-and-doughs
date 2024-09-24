8a61aa4804dc4419eaca66a1a62a09408af106fa
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-09-24 11:11:41 +0200

    basic product card to be improved later

---
+++ frontend-react/dos-and-doughs/src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,22 @@
+import './ProductCard.css';
+
+export type productCardProps = {
+    imageUrl: string, 
+    title: string, 
+    description?: string,
+    subtitle: string,
+    price: number | string
+}
+
+function ProductCard(props: productCardProps) {
+    return (
+        <div className="product-card img-styling" style={{backgroundImage:`url(${props.imageUrl})`}}>
+            <div className="cake-info">
+                <h1 className="cake-title">{props.title}</h1>
+                <p>{props.subtitle}</p>
+            </div>
+        </div>
+    )
+};
+
+export default ProductCard;
--- frontend-react/dos-and-doughs/src/components/ShopUI/ShopUI.tsx
+++ frontend-react/dos-and-doughs/src/components/ShopUI/ShopUI.tsx
@@ -1,9 +1,54 @@
+import React, {useEffect, useState} from 'react';
+import ProductCard from '../ProductCard/ProductCard';
 import './ShopUI.css';
 
+import { productCardProps } from '../ProductCard/ProductCard';
+
+// example products
+const products: productCardProps[] = [
+    {
+        title: 'Product 1', 
+        price: '$20', 
+        description:'some description', 
+        subtitle:'some subtitle', 
+        imageUrl: '/images/tiramisu.webp' 
+    },
+    {
+        title: 'Product 2', 
+        price: '$30', 
+        description:'some description', 
+        subtitle:'some subtitle', 
+        imageUrl: '/images/m-m-cookies.webp' 
+    }
+]
+
 function Shop() {
+    // state for products
+    const [products, setProducts] = useState<productCardProps[]>([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('/cakes-api.json'); 
+                if (!response.ok) {
+                    throw new Error('Something went wrong');
+                }
+                const data = await response.json(); 
+                setProducts(data);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                console.log('finished fetching');
+            }
+        }
+
+        fetchProducts()
+    }, [])
     return (
         <div className="shop">
-            <h1>SHOP</h1>
+            {products.map((product: productCardProps, index: number) => (
+                <ProductCard key={index} title={product.title} price={product.price} description={product.description} subtitle={product.subtitle} imageUrl={product.imageUrl} />
+            ))}
         </div>
     )
 }
