8da3b43956a03df009507621c4872db5cfa2c42a
JINGBY <hannes@gingby.se>
2024-09-30 16:17:19 +0200

    Add basic login/signup UI

--- frontend-react/dos-and-doughs/src/App.tsx
+++ frontend-react/dos-and-doughs/src/App.tsx
@@ -5,6 +5,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home.tsx'
 import Shop from './pages/shop.tsx'
 import Visit from './pages/visit.tsx'
+import Account from './pages/account.tsx'
 
 import Layout from './layout.tsx'
 
@@ -19,6 +20,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path='shop' element={<Shop />} />
           <Route path='visit' element={<Visit />} />
+          <Route path='account' element={<Account />} />
         </Route>
       </Routes>
     </Router>
