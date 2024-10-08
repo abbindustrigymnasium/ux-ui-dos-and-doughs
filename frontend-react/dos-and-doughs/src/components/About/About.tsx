13f38c3d94be4f3705143e8f0e3fda8c52d68ace
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2024-10-08 09:11:31 +0200

    change language

--- frontend-react/dos-and-doughs/src/components/About/About.tsx
+++ frontend-react/dos-and-doughs/src/components/About/About.tsx
@@ -11,27 +11,27 @@ function About() {
             <div className="about-header">
                 <div>
                     <SectionDivider />
-                    <h1>About Us</h1>
+                    <h1>Fun facts about us</h1>
                 </div>
                 <img className="script-text" src={scriptText} alt="script text" />
             </div>
             <div className="about-container">
                 <div className='about-grid'>
                     <div>
-                        <h2>01</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit at elit nec aliquet. Cras dapibus ipsum magna, nec porta orci finibus eget.</p>
+                        <h2>01 - The Beginning</h2>
+                        <p>Dos and Doughs was founded in 1723 at Cambridge University. In the early years, the bakery primarily served the town of Cambridge, but we eventually moved to campus and started catering to the students and staff.</p>
                     </div>
                     <div>
-                        <h2>02</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit at elit nec aliquet. Cras dapibus ipsum magna, nec porta orci finibus eget.</p>
+                        <h2>02 - The Name</h2>
+                        <p>Once the local Cambridge Bakery moved to campus, the owner wanted a new catchy name to make the shop stand out and further market to students. After many minutes of deliberation, Alwin Forslund came up with the name "Dos and Doughs".</p>
                     </div>
                     <div>
-                        <h2>03</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit at elit nec aliquet. Cras dapibus ipsum magna, nec porta orci finibus eget.</p>
+                        <h2>03 - The Pastries</h2>
+                        <p>Our most popular pastry is the chocolate chip cookie. We sell 4.2 trillion cookies every year. The cookies are baked using an ancient, secret recipe from 1493, when Columbus rediscovered the american contintent.</p>
                     </div>
                     <div>
-                        <h2>04</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit at elit nec aliquet. Cras dapibus ipsum magna, nec porta orci finibus eget.</p>
+                        <h2>04 - The Future</h2>
+                        <p>We hope to expand our business in the future and we hope to serve joy at over 100 locations at the end of the year. In order for students to afford our pastries, we will attempt to keep our prices low amid inflation.</p>
                     </div>
                 </div>
                 <img className='script-logo' src={scriptLogo} alt="script logo of dos and doughs" />
